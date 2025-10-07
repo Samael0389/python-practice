f3daf5a8f891b372d537a1c1a3df0e467edc801c
Samael0389 <yeartur321@gmail.com>
2025-10-07 20:14:38 +0200

    Add files with loops
    Add files with loops "for" and "while"

---
+++ count_until_stop.py
@@ -0,0 +1,10 @@
+i = input("Enter something(type 'stop' to quit): ")
+while i != 'stop':
+    print (f"You entered:{i}")
+    i = input("Enter something(type 'stop' to quit): ")
+if i == 'stop':
+    print("Goodbye!")
+elif i == 'STOP':
+    print("Goodbye!")
+elif i == 'Stop':
+    print("Goodbye!")
